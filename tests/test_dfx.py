529202b220e9c76ea57ac6be87277b4e7422b97d
mtaka <mtakadev@gmail.com>
2020-06-15 16:24:16 +0900

    fixtureを追加してテスト名を変更してみた

--- tests/test_dfx.py
+++ tests/test_dfx.py
@@ -14,11 +14,16 @@ def text1():
     B,M,75,70,80
     B,M,95,60,50
     """
+@pytest.fixture
+def df1(text1):
+    return df_from_text(text1, sep=',')
 
-def test_df(text1):
-
+def test_from_text(text1):
     df = df_from_text(text1, sep=',')
     assert type(df), "DataFrame"
     assert len(df), 8
     assert list(df.columns), 'cls gen math eng jap'.split()
 
+def test_df_1(df1):
+    assert len(df1), 8
+    assert list(df1.columns), 'cls gen math eng jap'.split()
